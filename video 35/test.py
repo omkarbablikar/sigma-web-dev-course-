139232ca228e314bed755740f575996649b136aa
omkarbablikar <80235683+omkarbablikar@users.noreply.github.com>
2024-02-28 17:55:35 +0530

    Add files via upload

---
+++ video 35/test.py
@@ -0,0 +1,17 @@
+A = "part one"
+B = "part two"
+C = A+B;
+print(C*3)
+
+A = "part"
+B = 1
+
+
+C = A + str(B)
+
+C = "{}-{}".format(A,B)
+print(C)
+
+string = "your name is {} and you are {} years old. You live in {} and you love {}"
+output = string.format(name, age, Nanded, love)
+print(output)
